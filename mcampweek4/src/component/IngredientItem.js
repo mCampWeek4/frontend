87239c60612500b1cee90adee7776dc519d41ff2
cs20200221 <moonx010@kaist.ac.kr>
2022-01-20 14:49:52 +0900

    .

---
+++ mcampweek4/src/component/IngredientItem.js
@@ -0,0 +1,7 @@
+export default function IngredientItem({name}) {
+    return(
+        <div>
+            <h>{name}</h>
+        </div>
+    );
+};
---
+++ mcampweek4/src/component/IngredientList.js
@@ -0,0 +1,9 @@
+import IngredientItem from './IngredientItem';
+
+export default function IngredientList() {
+    return(
+        <div>
+            <IngredientItem name={"볶음밥"}/>
+        </div>
+    );
+};
