f152db2e3ec0a531364d6ba222fffb077579c640
Seungku Kim <seungkukim@kaist.ac.kr>
2022-01-25 00:41:48 +0900

    resolve conflict

--- mcampweek4/src/pages/FoodSearch.js
+++ mcampweek4/src/pages/FoodSearch.js
@@ -2,11 +2,19 @@ import React, {useEffect, useState} from 'react';
 import HomeHeader from './../component/HomeHeader';
 
 export default function FoodSearch() {
+     const tabBarList = [{active: true, title: "레시피 검색", link:'/'}, {active: false, title: "다른 탭", link: '/FoodSearch'}];
     
+    c
+
+
 
     return (
+        <>
+        <HomeHeader tabBarList={tabBarList}/>
         <div>
             <h1>이제 다시 시작!</h1>
         </div>
+        </>
+        
     )
 }
